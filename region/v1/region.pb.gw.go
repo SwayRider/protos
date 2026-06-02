1b22f80704a973bf87b6f2ae6cd85db8540c231c
Maarten Heremans <maarten@hevanto.be>
2026-06-02 12:26:42 +0200

    Add FindRouteRegionPaths RPC with RegionPath message

--- region/v1/region.pb.gw.go
+++ region/v1/region.pb.gw.go
@@ -170,6 +170,33 @@ func local_request_RegionService_FindRegionPath_0(ctx context.Context, marshaler
 	return msg, metadata, err
 }
 
+func request_RegionService_FindRouteRegionPaths_0(ctx context.Context, marshaler runtime.Marshaler, client RegionServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var (
+		protoReq FindRouteRegionPathsRequest
+		metadata runtime.ServerMetadata
+	)
+	if err := marshaler.NewDecoder(req.Body).Decode(&protoReq); err != nil && !errors.Is(err, io.EOF) {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if req.Body != nil {
+		_, _ = io.Copy(io.Discard, req.Body)
+	}
+	msg, err := client.FindRouteRegionPaths(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+}
+
+func local_request_RegionService_FindRouteRegionPaths_0(ctx context.Context, marshaler runtime.Marshaler, server RegionServiceServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var (
+		protoReq FindRouteRegionPathsRequest
+		metadata runtime.ServerMetadata
+	)
+	if err := marshaler.NewDecoder(req.Body).Decode(&protoReq); err != nil && !errors.Is(err, io.EOF) {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	msg, err := server.FindRouteRegionPaths(ctx, &protoReq)
+	return msg, metadata, err
+}
+
 // RegisterRegionServiceHandlerServer registers the http handlers for service RegionService to "mux".
 // UnaryRPC     :call RegionServiceServer directly.
 // StreamingRPC :currently unsupported pending https://github.com/grpc/grpc-go/issues/906.
@@ -275,6 +302,26 @@ func RegisterRegionServiceHandlerServer(ctx context.Context, mux *runtime.ServeM
 			return
 		}
 		forward_RegionService_FindRegionPath_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+	})
+	mux.Handle(http.MethodPost, pattern_RegionService_FindRouteRegionPaths_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		var stream runtime.ServerTransportStream
+		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		annotatedContext, err := runtime.AnnotateIncomingContext(ctx, mux, req, "/region.v1.RegionService/FindRouteRegionPaths", runtime.WithHTTPPathPattern("/api/v1/region/find-route-region-paths"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := local_request_RegionService_FindRouteRegionPaths_0(annotatedContext, inboundMarshaler, server, req, pathParams)
+		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
+		annotatedContext = runtime.NewServerMetadataContext(annotatedContext, md)
+		if err != nil {
+			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		forward_RegionService_FindRouteRegionPaths_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 	})
 
 	return nil
@@ -401,6 +448,23 @@ func RegisterRegionServiceHandlerClient(ctx context.Context, mux *runtime.ServeM
 		}
 		forward_RegionService_FindRegionPath_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 	})
+	mux.Handle(http.MethodPost, pattern_RegionService_FindRouteRegionPaths_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		annotatedContext, err := runtime.AnnotateContext(ctx, mux, req, "/region.v1.RegionService/FindRouteRegionPaths", runtime.WithHTTPPathPattern("/api/v1/region/find-route-region-paths"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_RegionService_FindRouteRegionPaths_0(annotatedContext, inboundMarshaler, client, req, pathParams)
+		annotatedContext = runtime.NewServerMetadataContext(annotatedContext, md)
+		if err != nil {
+			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		forward_RegionService_FindRouteRegionPaths_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+	})
 	return nil
 }
 
@@ -410,6 +474,7 @@ var (
 	pattern_RegionService_SearchRadius_0          = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"api", "v1", "region", "search-radius"}, ""))
 	pattern_RegionService_FindCrossingLocations_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"api", "v1", "region", "find-crossing-locations"}, ""))
 	pattern_RegionService_FindRegionPath_0        = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"api", "v1", "region", "find-region-path"}, ""))
+	pattern_RegionService_FindRouteRegionPaths_0  = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"api", "v1", "region", "find-route-region-paths"}, ""))
 )
 
 var (
@@ -418,4 +483,5 @@ var (
 	forward_RegionService_SearchRadius_0          = runtime.ForwardResponseMessage
 	forward_RegionService_FindCrossingLocations_0 = runtime.ForwardResponseMessage
 	forward_RegionService_FindRegionPath_0        = runtime.ForwardResponseMessage
+	forward_RegionService_FindRouteRegionPaths_0  = runtime.ForwardResponseMessage
 )
